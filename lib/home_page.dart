1436d0ec2a3393d6a1cb65202fa3bfe300e1b124
1125bid <dlfdlfdldh1125@naver.com>
2024-12-26 13:14:09 +0900

    feat: 현재상영중 ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,20 +7,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
-        children: [
-          const Text('가장 인기 있는'),
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, '/detail_page');
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: [
+            const Text('가장 인기 있는'),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/detail_page');
+              },
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(20),
                 child: Container(
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(200)),
                   height: 500,
                   width: double.infinity,
                   child: Image.network(
@@ -30,11 +29,50 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+            const SizedBox(height: 10),
+            const Text('현재 상영중'),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 250,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  item(),
+                  item(),
+                  item(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class item extends StatelessWidget {
+  const item({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, '/detail_page');
+        },
+        child: AspectRatio(
+          aspectRatio: 1 / 1.5,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              'https://picsum.photos/seed/picsum/200/300',
+              fit: BoxFit.cover,
+            ),
           ),
-          // ListView(
-          //   scrollDirection: Axis.horizontal,
-          // ),
-        ],
+        ),
       ),
     );
   }
