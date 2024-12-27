9e0a0983940935e30aebda6f99dbc635f5a59069
1125bid <dlfdlfdldh1125@naver.com>
2024-12-27 13:35:20 +0900

    feat: 디테일페이지 ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -19,7 +19,7 @@ class HomePage extends StatelessWidget {
               },
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(20),
-                child: Container(
+                child: SizedBox(
                   height: 500,
                   width: double.infinity,
                   child: Image.network(
@@ -36,10 +36,38 @@ class HomePage extends StatelessWidget {
               height: 250,
               child: ListView(
                 scrollDirection: Axis.horizontal,
+                children: const [
+                  Item(),
+                  Item(),
+                  Item(),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text('인기순'),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 250,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
                 children: [
-                  item(),
-                  item(),
-                  item(),
+                  StackedItem(num: '1'),
+                  StackedItem(num: '2'),
+                  StackedItem(num: '3'),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text('평점 높은 순'),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 250,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  Item(),
+                  Item(),
+                  Item(),
                 ],
               ),
             ),
@@ -50,15 +78,52 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class item extends StatelessWidget {
-  const item({
+class StackedItem extends StatelessWidget {
+  StackedItem({super.key, required this.num});
+  String num;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/detail_page');
+      },
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 25),
+            child: AspectRatio(
+              aspectRatio: 1 / 1.5,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  'https://picsum.photos/seed/picsum/200/300',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              num,
+              style: TextStyle(fontSize: 100, color: Colors.grey[300]),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Item extends StatelessWidget {
+  const Item({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(right: 10),
+      padding: const EdgeInsets.only(right: 10),
       child: GestureDetector(
         onTap: () {
           Navigator.pushNamed(context, '/detail_page');
