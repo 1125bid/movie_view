79bbe2c86ae4c89e7856e60be9f14be25d928a30
1125bid <dlfdlfdldh1125@naver.com>
2024-12-30 19:30:52 +0900

    fix: UseCase 클래스 이름 변경

--- lib/domain/usecase/fetch_now_playing_movies_usecase.dart
+++ lib/domain/usecase/fetch_now_playing_movies_usecase.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_movie_view_app/domain/entity/movie.dart';
 import 'package:flutter_movie_view_app/domain/repository/movie_repository.dart';
 
-class FetchMovieUseCase {
-  FetchMovieUseCase(this._movieRepository);
+class FetchNowPlayingMovies {
+  FetchNowPlayingMovies(this._movieRepository);
   final MovieRepository _movieRepository;
 
   Future<List<Movie>?> fetchNowPlayingMovies() async {
--- lib/domain/usecase/fetch_upcoming.movies_usecase.dart
+++ lib/domain/usecase/fetch_upcoming.movies_usecase.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_movie_view_app/domain/entity/movie.dart';
 import 'package:flutter_movie_view_app/domain/repository/movie_repository.dart';
 
-class FetchMovieUseCase {
-  FetchMovieUseCase(this._movieRepository);
+class FetchUpcomingMoviesUseCase {
+  FetchUpcomingMoviesUseCase(this._movieRepository);
   final MovieRepository _movieRepository;
 
   Future<List<Movie>?> fetchUpcomingMovies() async {
