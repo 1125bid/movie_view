7d5ffa93732a099bed62aa153fd38cf6eb6633cc
1125bid <dlfdlfdldh1125@naver.com>
2024-12-30 19:19:18 +0900

    fix: usecase

--- lib/domain/usecase/fetch_movie_usecase.dart
+++ lib/domain/usecase/fetch_movie_usecase.dart
@@ -1,28 +1,28 @@
-import 'package:flutter_movie_view_app/data/dto/movie_detail_dto.dart';
-import 'package:flutter_movie_view_app/data/dto/movie_response_dto.dart';
-import 'package:flutter_movie_view_app/data/repository/movie_repository.dart';
+import 'package:flutter_movie_view_app/domain/entity/movie.dart';
+import 'package:flutter_movie_view_app/domain/entity/movie_detail.dart';
+import 'package:flutter_movie_view_app/domain/repository/movie_repository.dart';
 
 class FetchMovieUseCase {
   FetchMovieUseCase(this._movieRepository);
   final MovieRepository _movieRepository;
 
-  Future<MovieResponseDto?> fetchPopularMovies() async {
+  Future<List<Movie>?> fetchPopularMovies() async {
     return await _movieRepository.fetchPopularMovies();
   }
 
-  Future<MovieDetailDto?> fetchMovieDetail(int id) async {
+  Future<MovieDetail?> fetchMovieDetail(int id) async {
     return await _movieRepository.fetchMovieDetail(id);
   }
 
-  Future<MovieResponseDto?> fetchNowPlayingMovies() async {
+  Future<List<Movie>?> fetchNowPlayingMovies() async {
     return await _movieRepository.fetchNowPlayingMovies();
   }
 
-  Future<MovieResponseDto?> fetchTopRatedMovies() async {
+  Future<List<Movie>?> fetchTopRatedMovies() async {
     return await _movieRepository.fetchTopRatedMovies();
   }
 
-  Future<MovieResponseDto?> fetchUpcomingMovies() async {
+  Future<List<Movie>?> fetchUpcomingMovies() async {
     return await _movieRepository.fetchUpcomingMovies();
   }
 }
