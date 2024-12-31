5b5dbb70420cafa7cc246880772ad637b6381dc4
1125bid <dlfdlfdldh1125@naver.com>
2024-12-31 14:51:49 +0900

    feat: riverpod 설치 및 providerscope설정

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_movie_view_app/presentation/pages/detail_page.dart';
 import 'package:flutter_movie_view_app/presentation/pages/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
