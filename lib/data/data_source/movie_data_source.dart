77dd0d43da551cb1a5db42d1a6e75eb43de35ccf
1125bid <dlfdlfdldh1125@naver.com>
2024-12-31 14:53:56 +0900

    refactor: movie_data_source 주석제거

--- lib/data/data_source/movie_data_source.dart
+++ lib/data/data_source/movie_data_source.dart
@@ -5,11 +5,8 @@ import 'package:flutter_movie_view_app/data/dto/movie_response_dto.dart';
 abstract interface class MovieDataSource {
   Future<List<MovieResponseDto>?> fetchNowPlayingMovies();
 
-// FIXME List<MovieResponseDto>?
   Future<List<MovieResponseDto>?> fetchPopularMovies();
-// FIXME List<MovieResponseDto>?
   Future<List<MovieResponseDto>?> fetchTopRatedMovies();
-// FIXME List<MovieResponseDto>?
   Future<List<MovieResponseDto>?> fetchUpcomingMovies();
 
   Future<MovieDetailDto?> fetchMovieDetail(int id);
